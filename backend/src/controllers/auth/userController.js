8105a0f01e6bbc4bf989d943c7cfeae27da8192f
mizanade <mizanadearfan16@gmail.com>
2024-11-06 22:32:00 +0700

    feat: user login status

--- backend/src/controllers/auth/userController.js
+++ backend/src/controllers/auth/userController.js
@@ -2,6 +2,7 @@ import asyncHandler from "express-async-handler";
 import User from "../../models/auth/UserModel.js";
 import generateToken from "../../helpers/generateToken.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const registerUser = asyncHandler(async (req, res) => {
   const { name, username, email, password } = req.body;
@@ -167,8 +168,25 @@ export const updateUser = asyncHandler(async (req, res) => {
       bio: updated.bio,
       photo: updated.photo,
     });
-  }else {
+  } else {
     res.status(404);
     throw new Error("User not found!");
+  }
+});
+
+//login status
+export const userLoginStatus = asyncHandler(async (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    res.status(401).json({
+      message: "Not authorized, please login",
+    });
+  }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  if (decoded) {
+    res.status(200).json(true);
+  } else {
+    res.status(401).json(false);
   }
 });
